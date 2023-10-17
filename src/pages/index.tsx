61ff80836fad1ab5d92024008f7c844a99185c67
Gabriel C <gpcordeiro@hotmail.com>
2023-10-17 00:02:59 -0700

    Implemented Tab Change Confirmation and Timer Reset
    Added tab change confirmation dialog and implemented timer reset when changing tabs.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,8 @@ import TaskList from "@/app/components/TaskList";
 import Navigation from "@/app/components/Navigation";
 import Timer from "@/app/components/Timer";
 import {Dispatch, SetStateAction, useEffect, useState} from "react";
+import {Simulate} from "react-dom/test-utils";
+import reset = Simulate.reset;
 
 
 const Index = () => {
@@ -12,36 +14,51 @@ const Index = () => {
     const [shortBreak, setShortBreak] = useState<number>(5)
     const [seconds, setSeconds] = useState<number>(0);
     const [ticking, setTicking] = useState<boolean>(false);
-
+    const [consumedSecond, setConsumedSecond] = useState<number>(0);
 
     const [stage, setStage] = useState<number>(0)
 
     const switchStage = (index: number) => {
-        setStage(index)
+        const isYes = consumedSecond && stage !== index ? confirm("Are you sure you want to switch?") : false
+        if (isYes) {
+            reset();
+            setStage(index)
+        } else if (!consumedSecond) {
+            setStage(index)
+        }
     }
 
     const getTickingTime = () => {
         const timeStage: { [index: number]: number } = {
             0: pomodoro,
-            1: longBreak,
-            2: shortBreak,
+            1: shortBreak,
+            2: longBreak,
         }
         return timeStage[stage];
     }
     const updateMinute = () => {
         const updateStage: { [index: number]: Dispatch<SetStateAction<number>> } = {
             0: setPomodoro,
-            1: setLongBreak,
-            2: setShortBreak,
+            1: setShortBreak,
+            2: setLongBreak,
         }
         return updateStage[stage];
+    }
+
+    const reset = () => {
+        setTicking(false);
+        setSeconds(0);
+        setConsumedSecond(0);
+        setPomodoro(25);
+        setLongBreak(10);
+        setShortBreak(5);
     }
     const clockTicking = () => {
         const minutes = getTickingTime();
         const setMinutes = updateMinute();
 
         if (!minutes && !seconds) {
-            alert(" timer up")
+            reset();
         } else if (!seconds) {
             setMinutes((minute: number) => minute - 1);
             setSeconds(59);
@@ -54,6 +71,7 @@ const Index = () => {
     useEffect(() => {
         const timer = setInterval(() => {
             if (ticking) {
+                setConsumedSecond(value => value + 1)
                 clockTicking();
             }
         }, 1000);
